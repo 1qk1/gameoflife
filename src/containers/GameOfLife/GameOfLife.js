9e396e9d529228fa86ac98e81704581748043e75
Panagiotis <1qk231@gmail.com>
2019-05-21 19:16:02 +0300

    replace getAdjacent with getAlive, improve nextGen function

--- src/containers/GameOfLife/GameOfLife.js
+++ src/containers/GameOfLife/GameOfLife.js
@@ -1,10 +1,10 @@
-import React, { PureComponent } from 'react'
-import Controls from '../../components/Controls/Controls';
-import shapes from '../../shapes';
+import React, { PureComponent } from "react";
+import Controls from "../../components/Controls/Controls";
+import shapes from "../../shapes";
 
-import Grid from '../../components/Grid/Grid';
+import Grid from "../../components/Grid/Grid";
 
-import './GameOfLife.css';
+import "./GameOfLife.css";
 
 class GameOfLife extends PureComponent {
   state = {
@@ -13,7 +13,7 @@ class GameOfLife extends PureComponent {
     gridSize: [50, 100],
     speed: 150,
     placingShape: null
-  }
+  };
 
   componentDidMount() {
     this.initGrid();
@@ -21,23 +21,29 @@ class GameOfLife extends PureComponent {
 
   initGrid = () => {
     const [rows, columns] = [...this.state.gridSize];
-    const newGrid = Array(rows).fill(Array(columns).fill(false))
-    this.setState({grid: newGrid});
-  }
+    const newGrid = Array(rows).fill(Array(columns).fill(false));
+    this.setState({ grid: newGrid });
+  };
 
   initRandomGrid = () => {
     const [rows, columns] = [...this.state.gridSize];
-    const newGrid = Array(rows).fill().map(() => Array(columns).fill(false).map(() => Math.random() >= 0.5))
-    this.setState({grid: newGrid});
-  }
+    const newGrid = Array(rows)
+      .fill()
+      .map(() =>
+        Array(columns)
+          .fill(false)
+          .map(() => Math.random() >= 0.5)
+      );
+    this.setState({ grid: newGrid });
+  };
 
   placeAlive = cell => {
     const newGrid = [...this.state.grid].map(row => [...row]);
     newGrid[cell[0]][cell[1]] = !newGrid[cell[0]][cell[1]];
-    this.setState({grid: newGrid});
-  }
+    this.setState({ grid: newGrid });
+  };
 
-  getAdjacent = cell => {
+  getAlive = cell => {
     const [rows, columns] = [...this.state.gridSize];
     const adjacentCells = [
       [cell[0] - 1, cell[1] - 1],
@@ -47,100 +53,87 @@ class GameOfLife extends PureComponent {
       [cell[0] + 1, cell[1] + 1],
       [cell[0] + 1, cell[1]],
       [cell[0] + 1, cell[1] - 1],
-      [cell[0], cell[1] - 1],
-    ]
-    .filter(c => 
-      (c[0] >= 0 && c[0] < rows) 
-      && 
-      (c[1] >= 0 && c[1] < columns));
-    return adjacentCells;
-  }
+      [cell[0], cell[1] - 1]
+    ].filter(
+      c =>
+        c[0] >= 0 &&
+        c[1] >= 0 &&
+        (c[0] < rows && c[1] < columns) &&
+        this.state.grid[c[0]][c[1]]
+    );
+
+    return adjacentCells.length;
+  };
 
   nextGen = () => {
-    // make a grid for the future generation
-    const nextGenGrid = [...this.state.grid].map(row => [...row]);
-    // get the active cells on the grid
-    const activeCells = [];
-    this.state.grid.forEach((row, rowI) => {
-      row.forEach((cell, colI) => {
-        if (cell === true) {
-          activeCells.push([rowI, colI]);
-        }
-      })
-    });
+    const next = prevState => {
+      // make a grid for the future generation
+      const nextGenGrid = JSON.parse(JSON.stringify(prevState.grid));
 
-    // format and add all the dead adjacent cells
-    let cellsToCheck = activeCells.reduce((sum, b) => {
-      return sum.concat([b, ...this.getAdjacent(b)]);
-    }, []);
-    // filter so that each cell exists only once
-    cellsToCheck = cellsToCheck.reduce((sum, cell) => {
-      const existsInSum = sum.some(itterable => (
-        itterable[0] === cell[0] && itterable[1] === cell[1])
-      );
-      if (!existsInSum){
-        return [...sum, cell];
-      } else {
-        return sum;
-      }
-    }, []);
+      // compare cells to game rules
+      this.state.grid.forEach((row, rowIndex) => {
+        row.forEach((cell, cellIndex) => {
+          const aliveNeighbours = this.getAlive([rowIndex, cellIndex]);
+          if (cell === true) {
+            if (aliveNeighbours < 2 || aliveNeighbours > 3) {
+              nextGenGrid[rowIndex][cellIndex] = false;
+            }
+          } else {
+            if (aliveNeighbours === 3) {
+              nextGenGrid[rowIndex][cellIndex] = true;
+            }
+          }
+        });
+      });
 
-    // compare cells to game rules
-    cellsToCheck.forEach(cell => {
-      const cellStatus = this.state.grid[cell[0]][cell[1]];
-      const aliveNeighbours = this.getAdjacent(cell).filter(c => this.state.grid[c[0]][c[1]] === true).length
-      if (cellStatus === true) {
-        if (aliveNeighbours < 2 || aliveNeighbours > 3) {
-          nextGenGrid[cell[0]][cell[1]] = false;
-        }
-      } else {
-        if (aliveNeighbours === 3) {
-          nextGenGrid[cell[0]][cell[1]] = true;
-        }
-      }
-    });
-    this.setState({grid: nextGenGrid, genNo: this.state.genNo + 1});
-  }
+      return nextGenGrid;
+    };
+
+    this.setState(prevState => ({
+      grid: next(prevState),
+      genNo: this.state.genNo + 1
+    }));
+  };
 
   startGame = () => {
     clearInterval(this.timer);
     this.timer = setInterval(() => {
       this.nextGen();
     }, this.state.speed);
-  }
+  };
 
   pauseGame = () => {
     clearInterval(this.timer);
-  }
+  };
 
   clearGrid = () => {
     clearInterval(this.timer);
     this.initGrid();
-    this.setState({genNo: 0});
-  }
+    this.setState({ genNo: 0 });
+  };
 
   gridSizeHandler = size => {
     this.clearGrid();
-    this.setState({gridSize: size}, () => {
+    this.setState({ gridSize: size }, () => {
       this.initGrid();
-    })
-  }
+    });
+  };
 
   speedChangeHandler = newSpeed => {
-    this.setState({speed: newSpeed}, () => {
+    this.setState({ speed: newSpeed }, () => {
       const gameOn = this.timer !== undefined;
       if (gameOn === true) {
         this.startGame();
       }
     });
-  }
+  };
 
-  placeShape1 = (shape) => {
+  placeShape1 = shape => {
     // first phase when you click the shape from the dropdown
-    this.setState({placingShape: shape});
-  }
+    this.setState({ placingShape: shape });
+  };
 
-  placeShape2 = (centerCell) => {
+  placeShape2 = centerCell => {
     // second phase when you click a cell on the grid
     // takes center cell (function argument)
     // takes shape to place
@@ -152,28 +145,33 @@ class GameOfLife extends PureComponent {
     const [rows, columns] = this.state.gridSize;
     // calculates starting position around the center cell
     const [startingRow, startingColumn] = [
-      centerCell[0] - Math.floor(shapeToPlace.length / 2), 
+      centerCell[0] - Math.floor(shapeToPlace.length / 2),
       centerCell[1] - Math.floor(shapeToPlace[0].length / 2)
     ];
     // places shape
     shapeToPlace.forEach((row, rowIndex) => {
       // if it's out of range it just moves on #0hecks
-      if (rowIndex + startingRow >= 0 && rowIndex + startingRow < rows){
+      if (rowIndex + startingRow >= 0 && rowIndex + startingRow < rows) {
         row.forEach((column, columnIndex) => {
-          if (columnIndex + startingColumn >= 0 && columnIndex + startingColumn < columns) {
-            newGrid[rowIndex + startingRow][columnIndex + startingColumn] = column;
+          if (
+            columnIndex + startingColumn >= 0 &&
+            columnIndex + startingColumn < columns
+          ) {
+            newGrid[rowIndex + startingRow][
+              columnIndex + startingColumn
+            ] = column;
           }
-        })
+        });
       }
-    })
+    });
     // sets states back to new grid
-    this.setState({grid: newGrid, placingShape: null});
-  }
+    this.setState({ grid: newGrid, placingShape: null });
+  };
 
   render() {
     return (
-      <div className='GameOfLife'>
-      <h1>Conway&apos;s Game of Life</h1>
+      <div className="GameOfLife">
+        <h1>Conway&apos;s Game of Life</h1>
         <Controls
           speed={this.state.speed}
           speedChangeHandler={this.speedChangeHandler}
@@ -181,21 +179,21 @@ class GameOfLife extends PureComponent {
           startGame={this.startGame}
           clearGrid={this.clearGrid}
           pauseGame={this.pauseGame}
-          incSpeed={() => this.changeSpeed('inc')}
-          decSpeed={() => this.changeSpeed('dec')}
+          incSpeed={() => this.changeSpeed("inc")}
+          decSpeed={() => this.changeSpeed("dec")}
           gridSizeHandler={this.gridSizeHandler}
           placeShape={this.placeShape1}
         />
-        <Grid 
-          gridSize={this.state.gridSize} 
-          grid={this.state.grid} 
+        <Grid
+          gridSize={this.state.gridSize}
+          grid={this.state.grid}
           clicked={this.placeAlive}
           placingShape={this.state.placingShape}
           placeShape={this.placeShape2}
         />
         <h3>Generation: {this.state.genNo}</h3>
       </div>
-    )
+    );
   }
 }
 
